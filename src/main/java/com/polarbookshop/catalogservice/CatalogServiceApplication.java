38304fb9889d7b76c2cceae7b94f0b2d7c2d5f72
이지호 <leech2479@naver.com>
2025-06-18 23:48:21 +0900

    config server 세팅

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -2,8 +2,10 @@ package com.polarbookshop.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan // property bean 로드
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
